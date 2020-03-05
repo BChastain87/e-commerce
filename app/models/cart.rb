8d992574c537d9c9d1e15d6cece8e2a333c5b2f3
Brian Chastain <boush_0@hotmail.com>
2020-03-05 10:40:27 -0600

    Added to table with default value, removed parameter from permitted list, add css, add total_price to cart and quantity count to items

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,13 +1,17 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
 
-  def add_product(product_id)
-    current_item = line_items.find_by_product_id(product_id)
+  def add_product(product)
+    current_item = line_items.find_by(product_id: product.id)
     if current_item 
       current_item.quantity += 1
     else
-      current_item = line_items.build(:product_id => product_id)
+      current_item = line_items.build(product_id: product.id)
     end
-    return current_item
+    current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
   end
 end
